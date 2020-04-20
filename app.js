1f5458161340bbe25fd4af94cf205409e9fc0c8e
Ilyas Benhssine <ilyasbenhssine@gmail.com>
2020-04-20 23:09:07 +0000

    add base

---
+++ app.js
@@ -0,0 +1,19 @@
+
+var listpz=[['Buster.webp', 'Buster Meal', 2],['DoubleD-luxe.webp', 'DoubleD', 2]]
+
+function additem(pic, name, price,) {
+    
+{/* <div class="pz">
+    <img src="pizza/Buster.webp" width="150px">
+    <p>2$ - Buster Meal</p>
+    <input type="number" name="pricebuster" min="0"placeholder="Quantity">
+</div> */}
+    
+    var cont = getElementById("here");
+
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        
+    }
+
+}
